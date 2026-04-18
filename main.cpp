93b74f137e54ed6fa2c2e5f21c0488acb46801bd
Jesse Owen <jesseowenuk@googlemail.com>
2026-04-18 22:15:39 +0100

    Playing around with sockets

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <netdb.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <unistd.h>
+
+struct url_parts
+{
+    std::string scheme;
+    std::string host;
+    std::string port;
+    std::string path;
+};
+
+url_parts parse_url(std::string url)
+{
+    url_parts parts;
+    size_t scheme_end = url.find("://");
+
+    if(scheme_end != std::string::npos)
+    {
+        parts.scheme = url.substr(0, scheme_end);
+        url.erase(0, scheme_end + 3);   // remove the scheme
+    }
+
+    size_t path_start = url.find("/");
+    if(path_start != std::string::npos)
+    {
+        parts.path = url.substr(path_start);
+        url.erase(path_start);  // now 'url' is just host[:port]
+    }
+    else
+    {
+        parts.path = "/";
+    }
+
+    size_t port_start = url.find(":");
+    if(port_start != std::string::npos)
+    {
+        parts.host = url.substr(0, port_start);
+        parts.port = url.substr(port_start + 1);
+    }
+    else
+    {
+        parts.host = url;
+        parts.port = (parts.scheme == "https") ? "443" : "80";
+    }
+
+    return parts;
+}
+
+int main()
+{
+    url_parts parts = parse_url("http://www.thatjessebloke.co.uk");
+
+    addrinfo hints = {0};
+    addrinfo *result = {0};
+
+    hints.ai_family = AF_INET;            // Supports IPv4 
+    hints.ai_socktype = SOCK_STREAM;      // TCP connection
+
+    // use .c_str() to convert std::string to a style string which getaddrinfo needs
+    // this then uses getaddrinfo to do a DNS lookup
+    int status = getaddrinfo(parts.host.c_str(), parts.port.c_str(), &hints, &result);
+
+    if(status != 0)
+    {
+        std::cerr << "DNS Lookup failed: " << gai_strerror(status) << std::endl;
+        return 1;
+    }
+
+    // Create a socket
+    int sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if(sockfd == -1)
+    {
+        perror("Socket creation failed");
+        freeaddrinfo(result);
+        return 1;
+    }
+
+    // Connect to the server
+    if(connect(sockfd, result->ai_addr, result->ai_addrlen) == -1)
+    {
+        perror("Connection failed");
+        close(sockfd);
+        freeaddrinfo(result);
+        return 1;
+    }
+
+    std::cout << "Successfully connected to " << parts.host << " on port " << parts.port << std::endl;
+}
